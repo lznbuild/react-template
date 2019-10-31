d4f1bec9766c43b13d899d8ce5d95a240cfde935
qianjl <597187849@163.com>
2019-10-31 17:13:29 +0800

    升级

--- assets/components/Echart/index.jsx
+++ assets/components/Echart/index.jsx
@@ -1,75 +1,39 @@
 'use strict';
 
-import React from 'react';
-import { injectIntl /*, formatMessage*/ } from 'react-intl';
-
+import React, { useEffect } from 'react';
 import echarts from 'echarts/lib/echarts';
+import 'echarts/lib/chart/line';
 import 'echarts/lib/chart/bar';
-import 'echarts/lib/chart/radar';
-import 'echarts/lib/component/tooltip';
-import 'echarts/lib/component/legend';
-import 'echarts/lib/component/title';
+import 'echarts/lib/chart/pie';
 
-let uniqueId = Math.random();
-// eslint-disable-next-line no-plusplus
-const generateUniqueId = () => `rc-ec-${uniqueId++}`;
+const Echart = (props) => {
+  const { options, height } = props;
+  const chartId = `rc-ec-${Math.random()}`;
+  let chart;
 
-class ChartLineAnalysis extends React.Component {
-  constructor(props) {
-    super(props);
-    this.chart = null;
-    this.chartId = generateUniqueId();
-  }
-
-  componentDidMount() {
-    this.initChart(this.props);
-  }
-
-  componentWillReceiveProps(newProps) {
-    this.chart.clear();
-    this.chart.setOption(newProps.options);
-  }
-
-  componentWillUpdate() {
-    this.chart.clear();
-  }
-
-  shouldComponentUpdate() {
-    return false;
-  }
-
-  // componentWillUnmount() {
-  //   this.chart.dispose()
-  //   this.chart = null
-  //   this.chartId = ''
-  // }
-
-  initChart() {
-    const chartDom = document.getElementById(`${this.chartId}`);
-    //用于使chart自适应高度和宽度,通过窗体高宽计算容器高宽
-    const resizeChart = function() {
-      chartDom.style.height = `${(window.innerHeight - 470) / 2}px`;
-    };
+  const initChart = () => {
+    const chartDom = document.getElementById(`${chartId}`);
     //设置容器高宽
-    resizeChart();
-    const chart = echarts.init(chartDom);
-    chart.setOption(this.props.options);
-    if (this.props.options) {
-      // tools.loopShowTooltip(chart, this.props.options, { loopSeries: true }); // 使用本插件
-    }
-    this.chart = chart;
-    window.addEventListener(
-      'resize',
-      () => {
-        resizeChart();
-        chart.resize();
-      },
-      false
-    );
-  }
+    chartDom.style.height = height;
+    chart = echarts.init(chartDom);
+    chart.setOption(options);
+  };
 
-  render() {
-    return <div id={this.chartId} />;
-  }
-}
-export default injectIntl(ChartLineAnalysis);
+  // 用于使chart自适应高度和宽度,通过窗体高宽计算容器高宽
+  const resizeChart = () => {
+    const chartDom = document.getElementById(`${chartId}`);
+    chartDom.style.height = height;
+    chart.resize();
+  };
+
+  useEffect(() => {
+    initChart();
+    window.addEventListener('resize', resizeChart);
+    return () => {
+      window.removeEventListener('resize', resizeChart);
+    };
+  }, []);
+
+  return <div id={chartId} />;
+};
+export default Echart;
